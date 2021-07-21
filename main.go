9dd61ccf6f411bbd9dcb6e005434162c1551501b
dengjiawen8955@gmail.com <dengjiawen8955@gmail.com>
2021-07-21 21:49:41 +0800

    v2

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/dengjiawen8955/go_utils/restfulu"
+	v1 "github.com/dengjiawen8955/goweb/v1"
+)
+
+func main() {
+	w := v1.NewWeb("/v1")
+	w.Get("/ping", func(ctx *v1.Context) {
+		cl := ctx.Headers["content-length"]
+		fmt.Printf("%s\n", cl)
+		ctx.Json(restfulu.Ok("PONG"))
+	})
+	w.RunHTTP(8000)
+}
